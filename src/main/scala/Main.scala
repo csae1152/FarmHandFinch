b967ce939c8b1edc922add3d8f2925174fef5759
Helmut Steiner <helmut.steinersti2@gmail.com>
2018-06-28 10:46:00 +0200

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -23,7 +23,7 @@ object Main extends App {
 
   //asynchrone call
   val api: Endpoint[String] = get("OK") { Ok("FarmHand started.") }
-  Await.ready(Http.serve(":9000", api.toService))
+  Await.ready(Http.serve(":8080", api.toService))
 
 
 
